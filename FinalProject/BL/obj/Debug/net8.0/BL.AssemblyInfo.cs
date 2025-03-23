fd51a8b99c45c7f622259fd934c24e2448aa66af
RachelFried1 <rachelfried0828@gmail.com>
2025-03-23 15:26:31 +0200

    uploading project

--- FinalProject/BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
+++ FinalProject/BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001b2a6486416fbfd2150a6c7ea7aa0e531b0050")]
 [assembly: System.Reflection.AssemblyProductAttribute("BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
