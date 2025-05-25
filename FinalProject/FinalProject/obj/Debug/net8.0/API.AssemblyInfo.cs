174fe8001717112c883b5c74b7b39f5d77713d36
RachelFried1 <rachelfried0828@gmail.com>
2025-05-25 19:00:01 +0300

    Finalizing merge and including VS-generated changes.

--- FinalProject/FinalProject/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ FinalProject/FinalProject/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+422cff25a328d1f0b699050b1e9e4b33e0ba1589")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+086afece9aae840edc60ae8b174a881dcbe11e6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
